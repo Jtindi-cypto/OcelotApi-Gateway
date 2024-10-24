20da6c67b1faeba7ec37815febef0aeee1e518a5
Jacob Tindi <61144567+Jtindi-cypto@users.noreply.github.com>
2024-10-24 09:53:26 +0000

    Ocelot.json setup

--- OcelotWebApiGetaway/Program.cs
+++ OcelotWebApiGetaway/Program.cs
@@ -1,9 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//add Ocelot
+builder.Services.AddOcelot;
+
+//load the configurations from app seetings.json
+builder.Configuration.AddJsonFile("Ocelot.json");
+
 
 var app = builder.Build();
 
@@ -35,6 +45,10 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+//use ocelot middleware
+
+await app.UseOcelot();
 
 app.Run();
 
